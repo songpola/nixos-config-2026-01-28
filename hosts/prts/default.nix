66d11dfb3f8b39128acac3e430fbed2ebc2d65ea
Songpol Anannetikul <ice.songpola@pm.me>
2026-03-14 06:53:07 +0700

    refactor(prts): refactor Tailscale config into separate file

--- hosts/prts/default.nix
+++ hosts/prts/default.nix
@@ -27,31 +27,4 @@ delib.host {
 
   # GTX 1050 Ti does not support open-source kernel module
   myconfig.gpu.nvidia.useOpenSourceKernelModule = false;
-
-  # Network optimizations
-  myconfig.settings.netdevFeatures = {
-    enable = true;
-    interfaceFeatures."eno1" = {
-      # For Tailscale
-      # https://tailscale.com/kb/1320/performance-best-practices#linux-optimizations-for-subnet-routers-and-exit-nodes
-      # https://tailscale.com/blog/quic-udp-throughput
-      rx-udp-gro-forwarding = true;
-      rx-gro-list = false;
-
-      # To enable `generic-segmentation-offload`,
-      # these dependencies need to be enabled for it to be auto-enabled.
-      scatter-gather = true;
-      tcp-segmentation-offload = true;
-    };
-  };
-
-  # Enable Tailscale with subnet routing and exit node features
-  myconfig.services.tailscale.enable = true;
-  nixos.services.tailscale = {
-    useRoutingFeatures = "server";
-    extraSetFlags = [
-      "--advertise-routes=10.0.0.0/16"
-      "--advertise-exit-node"
-    ];
-  };
 }
