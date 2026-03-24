d13c70a0e32e1151bb792aa514ed1fd867ad7422
Songpol Anannetikul <ice.songpola@pm.me>
2026-03-24 14:23:21 +0700

    fix: fix firewalld config for Tailscale exit node and subnet routing

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -2,6 +2,8 @@
   delib,
   homeManagerUser,
   pkgs,
+  config,
+  lib,
   ...
 }:
 delib.module {
@@ -21,6 +23,32 @@ delib.module {
         # https://github.com/tailscale/tailscale/issues/5295
         "--ssh"
       ];
+    };
+
+    # Ref: https://github.com/tailscale/tailscale/issues/4639#issuecomment-1492942183
+    services.firewalld.zones = {
+      # Accept all inbound traffic from Tailscale
+      trusted.interfaces = [ config.services.tailscale.interfaceName ];
+      # Allow outbound traffic from Tailscale to the internet
+      # (masquerading as the host's IP address)
+      nixos-fw-default =
+        lib.mkIf
+          (
+            config.services.tailscale.useRoutingFeatures == "server"
+            || config.services.tailscale.useRoutingFeatures == "both"
+          )
+          {
+            masquerade = true;
+
+            # Syntax: https://www.xml.com/pub/a/2006/05/31/converting-between-xml-and-json.html
+            # Converter: https://pypi.org/project/xmltodict/
+            rules = [
+              {
+                "@family" = "ipv6"; # an argument
+                "masquerade" = ""; # an empty tag (content)
+              }
+            ];
+          };
     };
   };
 }
